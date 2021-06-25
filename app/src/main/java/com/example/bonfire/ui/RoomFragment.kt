43b82018cab519292e81b02cdd87b61af30cb536
Rohit Sharma <65807152+Rohit-2602@users.noreply.github.com>
2021-06-25 12:37:38 +0530

    Add Bottom Nav

---
+++ app/src/main/java/com/example/bonfire/ui/RoomFragment.kt
@@ -0,0 +1,26 @@
+package com.example.bonfire.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.bonfire.R
+import com.example.bonfire.databinding.FragmentHomeBinding
+import com.example.bonfire.databinding.FragmentRoomBinding
+
+class RoomFragment: Fragment(R.layout.fragment_room) {
+
+    private var _binding : FragmentRoomBinding ?= null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentRoomBinding.bind(view)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
